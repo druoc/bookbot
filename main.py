58914a08a63b15d94d5e2ee8f959198eda9a38b5
Dave <d.ruocco1987@googlemail.com>
2024-11-07 15:26:14 +0000

    Added report function

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 def main():
         book_text = get_book_text()
-        book_char_count = count_characters(book_text)
-        report(book_char_count)
+        book_word_count = word_count(book_text)
+        book_count_dict = count_characters(book_text)
+        book_chars_list_sorted = sort_dicts_to_list(book_count_dict)
+
+        report(book_word_count, book_chars_list_sorted)
         
 def get_book_text():
     with open("books/frankenstein.txt") as book:
         file_contents = book.read()
-        return file_contents
-                
+        return file_contents         
              
 def word_count(book):
     count_of_words = len(book.split())
-    print(f"This book contains {count_of_words} words")
+    return count_of_words
         
 def count_characters(book):
     lowercase_book = book.lower()
@@ -26,8 +28,27 @@ def count_characters(book):
                     
     return(char_count)
 
-def report(char_dict):
+def sort_on(d):
+    return d["num"]
+
+def sort_dicts_to_list(chars_dict):
+    sorted_list = []
+    for char in chars_dict:
+        sorted_list.append({"char": char, "num": chars_dict[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def report(num_of_words, book_chars_list):
     print("---Beginning book report---")
+    print(f"This book contains {num_of_words} words")
+    
+    for item in book_chars_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+        
+    print("---End report---")
+    
     
 
 
